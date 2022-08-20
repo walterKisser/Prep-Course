18833e6b28624cffbb7406650d2c61a149985d57
walterkisser <walterkisser2018@gmail.com>
2022-08-20 11:44:23 -0300

    repasando todo

--- 02-JS-I/homework/tests/JSI.test.js
+++ 02-JS-I/homework/tests/JSI.test.js
@@ -238,7 +238,7 @@ describe('combinarNombres(firstName, lastName)', function() {
   });
 });
 
-describe('obtenerSaludo(name)', function() {
+describe('obtenerSaludo(name', function() {
   it('Deberia devolver el string \'Hola {name}!\'', function() {
     expect(obtenerSaludo('Martin')).toBe('Hola Martin!');
     expect(obtenerSaludo('Antonio')).toBe('Hola Antonio!');
